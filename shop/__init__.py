de700a805897dcae3929bb75b8373c625ce2f525
Noah Tsegay <noahtsegay8@gmail.com>
2023-06-05 13:12:01 +0300

    'Update'

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 # create the app
 app = Flask(__name__)
 # configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///myshop.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myshop.db'
+app.config['SECRET_KEY'] = 'asdbasbdlkajsblda132d'
 # create the extension
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
-from shop import routes
+from shop.admin import routes 
--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -1,14 +1,24 @@
-from flask import render_template, session, request, redirect, url_for
+from flask import render_template, session, request, redirect, url_for, flash
 
-from shop import app, db
-
+from shop import app, db, bcrypt
+from .forms import RegistrationForm
+from .models import User
+import os
 
 @app.route('/')
 def home():
     return "Home page of RetailRealm"
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('admin/register.html', title="Register user")
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        hash_password = bcrypt.generate_password_hash(form.password.data)
+        user = User(name=form.name.data, username=form.username.data, email=form.email.data,
+                    password=hash_password)
+        db.session.add(user) 
+        flash('Thanks for registering')
+        return redirect(url_for('home'))
+    return render_template('admin/register.html', form=form, title="Registration page")
  
